5833aa5c568bdbeb1b39c1d5276669663427e981
omar <omar1996210@gmail.com>
2021-05-31 02:56:47 +0300

    update card style

--- src/app/pages/AddUsers/AddUsers.component.ts
+++ src/app/pages/AddUsers/AddUsers.component.ts
@@ -65,15 +65,15 @@ export class AddUsersComponent implements OnInit {
     onFileSelected(event: any) {
         this.selectedFile = <File>event.target.files[0];
 
-        // if (event.target.files && event.target.files[0]) {
-        //     var reader = new FileReader();
+        if (event.target.files && event.target.files[0]) {
+            var reader = new FileReader();
 
-        //     reader.readAsDataURL(event.target.files[0]); // read file as data url
+            reader.readAsDataURL(event.target.files[0]); // read file as data url
 
-        //     reader.onload = (event) => { // called once readAsDataURL is completed
-        //         this.url = event.target.result;
-        //     }
-        // }
+            reader.onload = (event) => { // called once readAsDataURL is completed
+                this.url = event.target.result;
+            }
+        }
     }
 
 }
